29cf2c9455e4be404f7ed1a7cb3f0cf01882b5b0
Maciek Misztal <mmisztal1980@gmail.com>
2024-12-20 23:56:26 +0100

    fix: formatting

--- src/CloudTek.Build/Utilities/MSBuildFileHandler.cs
+++ src/CloudTek.Build/Utilities/MSBuildFileHandler.cs
@@ -5,82 +5,79 @@ using Nuke.Common.IO;
 
 namespace CloudTek.Build.Utilities;
 
-  /// <summary>
-  /// Utility used to parse Directory.Build.(props | targets)
-  /// </summary>
-  internal class MsBuildFileHandler : XmlFileHandler
+/// <summary>
+/// Utility used to parse Directory.Build.(props | targets)
+/// </summary>
+internal class MsBuildFileHandler : XmlFileHandler
+{
+  private const string PropertyGroup = "PropertyGroup";
+
+#pragma warning disable CA1822
+  public IEnumerable<string> GetPinnedPackageVersion(AbsolutePath path)
+#pragma warning restore CA1822
   {
-    private const string PropertyGroup = "PropertyGroup";
-    // internal MsBuildFileHandler(AbsolutePath path)
-    // {
-    //   _ = path ?? throw new ArgumentNullException(nameof(path));
-    //   _document = GetDocument(path);
-    // }
-#pragma warning disable CA1822
-    public IEnumerable<string> GetPinnedPackageVersion(AbsolutePath path)
-#pragma warning restore CA1822
-    {
-      var doc = GetDocument(path);
-      var elements = GetElements(doc, "/Project/ItemGroup/PackageVersion")
-        .Where(element => element.Attribute("Include") != null)
-        .Where(element => element.Attribute("Version")?.Value.ToString().StartsWith('[') ?? false);
+    var doc = GetDocument(path);
+    var elements = GetElements(doc, "/Project/ItemGroup/PackageVersion")
+      .Where(element => element.Attribute("Include") != null)
+      .Where(element => element.Attribute("Version")?.Value.ToString().StartsWith('[') ?? false);
 
-      return elements.Select(element => element.Attribute("Include")!.Value);
-    }
+    return elements.Select(element => element.Attribute("Include")!.Value);
+  }
 
 #pragma warning disable CA1822
-    public IEnumerable<string> GetPinnedPackageReference(AbsolutePath path)
+  public IEnumerable<string> GetPinnedPackageReference(AbsolutePath path)
 #pragma warning restore CA1822
-    {
-      var doc = GetDocument(path);
-      var elements = GetElements(doc, "/Project/ItemGroup/PackageReference")
-        .Where(element => element.Attribute("Include") != null)
-        .Where(element => element.Attribute("Version")?.Value.ToString().StartsWith('[') ?? false);
+  {
+    var doc = GetDocument(path);
+    var elements = GetElements(doc, "/Project/ItemGroup/PackageReference")
+      .Where(element => element.Attribute("Include") != null)
+      .Where(element => element.Attribute("Version")?.Value.ToString().StartsWith('[') ?? false);
 
-      return elements.Select(element => element.Attribute("Include")!.Value);
+    return elements.Select(element => element.Attribute("Include")!.Value);
+  }
+
+  public static bool GetIsPackable(AbsolutePath path)
+  {
+    var doc = GetDocument(path);
+    var element = GetElements(doc, "/Project/PropertyGroup/IsPackable")
+      .SingleOrDefault();
+
+    return element != null && bool.Parse(element.Value);
+  }
+
+  public static void SetIsPackable(AbsolutePath path, bool value, bool createIfNotExists = true,
+    AbsolutePath? output = null)
+  {
+    var doc = GetDocument(path: path, createIfNotExists: createIfNotExists);
+    var root = doc.Root;
+
+    if (root == null)
+    {
+      throw new InvalidOperationException("Root element <Project /> is missing");
     }
 
-    public static bool GetIsPackable(AbsolutePath path)
+    var isPackable = GetIsPackable(path);
+
+    if (isPackable != value)
     {
-      var doc = GetDocument(path);
-      var element = GetElements(doc, "/Project/PropertyGroup/IsPackable")
-        .SingleOrDefault();
-
-      return element != null && bool.Parse(element.Value);
-    }
-
-    public static void SetIsPackable(AbsolutePath path, bool value, bool createIfNotExists = true, AbsolutePath? output = null)
-    {
-      var doc = GetDocument(path: path, createIfNotExists: createIfNotExists);
-      var root = doc.Root;
-
-      if (root == null)
+      var pg = root!.Element(PropertyGroup);
+      if (pg == null)
       {
-        throw new InvalidOperationException("Root element <Project /> is missing");
+        pg = new XElement(PropertyGroup);
+        root.Add(pg);
       }
 
-      var isPackable = GetIsPackable(path);
-
-      if (isPackable != value)
-      {
-        var pg = root!.Element(PropertyGroup);
-        if (pg == null)
-        {
-          pg = new XElement(PropertyGroup);
-          root.Add(pg);
-        }
-
-        pg.SetElementValue("IsPackable", value);
-      }
-
-      var settings = new XmlWriterSettings
-      {
-        OmitXmlDeclaration = true,
-        Indent = true,
-        Encoding = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false)
-      };
-
-      using var xw = XmlWriter.Create(output ?? path, settings);
-      doc.Save(xw);
+      pg.SetElementValue("IsPackable", value);
     }
+
+    var settings = new XmlWriterSettings
+    {
+      OmitXmlDeclaration = true,
+      Indent = true,
+      Encoding = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false)
+    };
+
+    using var xw = XmlWriter.Create(output ?? path, settings);
+    doc.Save(xw);
+  }
 }
