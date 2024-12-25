dc216ad8f44b11cfc6b9f512ff88a03850fb1a41
Maciek Misztal <mmisztal1980@gmail.com>
2024-12-25 12:40:08 +0100

    chore: old tests removed

--- tests/CloudTek.Build.Tests.Old/SmartBuildAccessor.cs
+++
@@ -1,9 +0,0 @@
-using System.Runtime.CompilerServices;
-
-namespace CloudTek.Build.Tests;
-
-public class SmartBuildAccessor
-{
-  [UnsafeAccessor(UnsafeAccessorKind.StaticMethod, Name = "MainEntryPoint")]
-  public static extern ref int GetMain(TestBuild build);
-}
